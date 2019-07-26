208fccf9719aa5c7fc95d6d3a3a6460d7123bd6b
João Gabriel Freitas <jgabrielafreitas@gmail.com>
2019-07-25 22:41:16 -0300

    add and configure swagger

---
+++ ZeDeX/DI/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZeDeX.DI
+{
+    public class DependencyInjection
+    {
+        public static void Inject(IConfiguration configuration, IServiceCollection services)
+        {
+        }
+    }
+}
--- ZeDeX/Startup.cs
+++ ZeDeX/Startup.cs
@@ -10,22 +10,41 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using ZeDeX.DI;
 
 namespace ZeDeX
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                                    {
+                                        Title = "Zedex API",
+                                        Version = "v1",
+                                        Description = "A partner CRUD",
+                                        Contact = new Contact
+                                        {
+                                            Name = "João Freitas",
+                                            Url = "https://github.com/jgabrielfreitas/"
+                                        }
+                                    });
+            });
+
+            DependencyInjection.Inject(_configuration, services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +62,13 @@ namespace ZeDeX
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(action =>
+            {
+                action.SwaggerEndpoint("/swagger/v1/swagger.json", "Conversor de Temperaturas");
+                action.RoutePrefix = string.Empty;
+            });
         }
     }
 }
