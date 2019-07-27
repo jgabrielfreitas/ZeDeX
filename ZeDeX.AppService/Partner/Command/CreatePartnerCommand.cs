5b95a174cc16be3f81a07e6e1803e22ed624553f
João Freitas <joao.freitas@nibo.com.br>
2019-07-27 20:48:26 -0300

    add create partner command and partner application service

---
+++ ZeDeX.AppService/Partner/Command/CreatePartnerCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeDeX.AppService.Partner.Command
+{
+    public class CreatePartnerCommand
+    {
+        public Owner owner { get; set; }
+        public Pdv pdv { get; set; }
+    }
+
+    public class Owner
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public class CoverageArea
+    {
+        public string type { get; set; }
+        public List<List<List<List<int>>>> coordinates { get; set; }
+    }
+
+    public class Address
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+
+    public class Pdv
+    {
+        public string document { get; set; }
+        public string name { get; set; }
+        public CoverageArea coverageArea { get; set; }
+        public Address address { get; set; }
+    }
+}
---
+++ ZeDeX.AppService/Partner/IPartnerAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZeDeX.AppService.Partner.Command;
+
+namespace ZeDeX.AppService.Partner
+{
+    public interface IPartnerAppService
+    {
+        Task CreatePartner(CreatePartnerCommand command);
+    }
+}
