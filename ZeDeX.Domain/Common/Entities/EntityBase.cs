9d9e98bcac28b3bde528b1c221599c889f362c10
João Freitas <joao.freitas@nibo.com.br>
2019-07-27 20:47:49 -0300

    add entity base

---
+++ ZeDeX.Domain/Common/Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeDeX.Domain.Common.Entities
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
