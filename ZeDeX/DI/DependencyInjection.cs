6a6908b891504d7af0762058ee671d8efb3375cf
João Freitas <joao.freitas@nibo.com.br>
2019-07-27 20:48:54 -0300

    add partner dependency injection

--- ZeDeX/DI/DependencyInjection.cs
+++ ZeDeX/DI/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ZeDeX.AppService.Partner;
 
 namespace ZeDeX.DI
 {
@@ -8,6 +9,12 @@ namespace ZeDeX.DI
     {
         public static void Inject(IConfiguration configuration, IServiceCollection services)
         {
+            #region app services
+
+            services.AddScoped<IPartnerAppService, PartnerAppService>();
+
+            #endregion
+
         }
     }
 }
