d6e83770bba6ee9ff502359c1b336be43eb4a622
João Freitas <joao.freitas@nibo.com.br>
2019-07-28 01:03:58 -0300

    add database connection and context

---
+++ ZeDeX.Infrastructure/EntityFramework/ZedexContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZeDeX.Domain.Common;
+using ZeDeX.Infrastructure.EntityFramework.DbMap.Address;
+using ZeDeX.Infrastructure.EntityFramework.DbMap.CoverageArea;
+using ZeDeX.Infrastructure.EntityFramework.DbMap.Emploer;
+using ZeDeX.Infrastructure.EntityFramework.DbMap.Partner;
+using ZeDeX.Infrastructure.EntityFramework.PersistenceModel.Address;
+using ZeDeX.Infrastructure.EntityFramework.PersistenceModel.CoverageArea;
+using ZeDeX.Infrastructure.EntityFramework.PersistenceModel.Employer;
+using ZeDeX.Infrastructure.EntityFramework.PersistenceModel.Partner;
+
+namespace ZeDeX.Infrastructure.EntityFramework
+{
+    public class ZedexContext : DbContext, IUnitOfWork
+    {
+        public DbSet<AddressPersistenceModel> PartnerAddresses { get; set; }
+        public DbSet<CoverageAreaPersistenceModel> CoverageAreas { get; set; }
+        public DbSet<EmployerPersistenceModel> Employees { get; set; }
+        public DbSet<PartnerPersistenceModel> Partners { get; set; }
+        private readonly string _connectionString;
+
+        public ZedexContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new PartnerDbMap().ModelBuilder(modelBuilder);
+            new AddressDbMap().ModelBuilder(modelBuilder);
+            new CoverageAreaDbMap().ModelBuilder(modelBuilder);
+            new EmployerDbMap().ModelBuilder(modelBuilder);
+
+        }
+
+        public async Task CommitAsync()
+        {
+            await SaveChangesAsync();
+        }
+    }
+}
--- ZeDeX/DI/DependencyInjection.cs
+++ ZeDeX/DI/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ZeDeX.AppService.Partner;
+using ZeDeX.Domain.Common;
+using ZeDeX.Infrastructure.EntityFramework;
 
 namespace ZeDeX.DI
 {
@@ -9,6 +12,13 @@ namespace ZeDeX.DI
     {
         public static void Inject(IConfiguration configuration, IServiceCollection services)
         {
+            #region database
+
+            services.AddDbContext<ZedexContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IUnitOfWork>(provider => (ZedexContext) provider.GetService(typeof(ZedexContext)));
+
+            #endregion
+
             #region app services
 
             services.AddScoped<IPartnerAppService, PartnerAppService>();
