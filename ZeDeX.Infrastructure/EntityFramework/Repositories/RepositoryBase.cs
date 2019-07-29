ba28debc3381a54ddddfd4b71549af38ff862c11
João Freitas <joao.freitas@nibo.com.br>
2019-07-28 23:20:00 -0300

    create partner

---
+++ ZeDeX.Infrastructure/EntityFramework/Repositories/RepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZeDeX.Domain.Common.Entities;
+
+namespace ZeDeX.Infrastructure.EntityFramework.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        protected readonly ZedexContext _context;
+
+        public RepositoryBase(ZedexContext context)
+        {
+            _context = context;
+        }
+    }
+}
